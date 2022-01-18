d20b9b887ac930af2c2d08480c04235525e234af
hiskiapp <hiskianggi@gmail.com>
2022-01-18 16:36:07 +0700

    Initial commit

---
+++ app/src/main/java/app/hiskia/advapp/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package app.hiskia.advapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.hiskia.advapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
