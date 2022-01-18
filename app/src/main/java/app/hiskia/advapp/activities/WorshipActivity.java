ca7588a3f067fc3577e8e10f1424be037adababe
hiskiapp <hiskianggi@gmail.com>
2022-01-18 18:31:20 +0700

    Initial commit

---
+++ app/src/main/java/app/hiskia/advapp/activities/WorshipActivity.java
@@ -0,0 +1,16 @@
+package app.hiskia.advapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.hiskia.advapp.R;
+
+public class WorshipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worship);
+    }
+}
